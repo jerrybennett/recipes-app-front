31fe41646a4e343f739292b49804f0c30bd34b44
jerrybennett <jerrysbusiness@gmail.com>
2018-03-01 17:20:26 -0500

    work on fetch

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default class Button extends React.Component {
-
+  handleClick = () => {
+    fetch(`http://locahost:3000/api/v1/recipes`).then(res => res.json()).then(console.log)
+  }
 
   render() {
     return (
       <div>
-        <button>Fetch!</button>
+        <button onClick={this.handleClick}>Fetch!</button>
       </div>
     );
   }
